3b6fd8ea0598fa833061bcddeb83ec30fddb525e
Alex-Casagrande-51 <alexcasagrandesia@gmail.com>
2022-06-21 11:52:17 -0300

    APREDENDO JS

---
+++ ex005/ex001.js
@@ -0,0 +1,9 @@
+var vel = 50.5
+console.log(`A velocidade do seu carro é de ${vel} km/h`)
+if (vel > 60) { // CONDIÇÃO SIMPLES 
+    console.log(`Você ultrapassou a velocidade permetida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
+
+
+
