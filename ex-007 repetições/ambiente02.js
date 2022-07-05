887c59aad70909b7207802e6356dc85937138c3b
Alex-Casagrande-51 <alexcasagrandesia@gmail.com>
2022-07-05 08:28:04 -0300

    modificações

--- ex-007 repetições/ambiente02.js
+++ ex-007 repetições/ambiente02.js
@@ -1,5 +1,17 @@
-var c = 1
-while (c <= 5) {
+
+console.log('Vai começar.....')
+for(var c = 1; c <= 6; c++) {
+    console.log(c)
+}
+console.log('Fim')
+
+
+
+
+
+/* var c = 1
+while (c <= 6) {
     console.log(c)
     c++
-}
+} 
+*/
