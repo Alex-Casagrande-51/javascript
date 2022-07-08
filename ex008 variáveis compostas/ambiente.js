ddec7b2078f52676403424345bae00a54093293d
Alex-Casagrande-51 <alexcasagrandesia@gmail.com>
2022-07-08 11:19:07 -0300

    Modificações

--- ex008 variáveis compostas/ambiente.js
+++ ex008 variáveis compostas/ambiente.js
@@ -1,3 +1,23 @@
-let num = [5, 8, 2, 9, 3]
 
+
+let num = [9, 6, 2, 5, 8, 7]
+num.push(1)
+num.sort()
+console.log(num)
+
+console.log(`O vetor tem ${num.length} prossião`)
+console.log(`O primeiro valor é ${num[0]}`)
+let pos = num.indexOf(3) 
+    if (pos == -1) {
+        console.log(`O valor não foi encontrado`)
+    } else {
+    console.log(`O valor 3 está na posição ${pos}`)
+}
+
+
+/*let num = [5, 8, 2, 9, 3]
+num [5] = 6
+num.push(1)
+num.sort()
 console.log(`Nosso vetor é ${num}`)
+console.log(`E também tem ${num.length} posições`)*/
---
+++ ex008 variáveis compostas/vetornatela.js
@@ -0,0 +1,12 @@
+let valores = [ 9,7, 4, 3, 2]
+valores.sort()
+console.log(valores)
+for (let pos in valores) {
+    console.log(`a posição ${pos} tem o valor ${valores[pos]}`)
+}
+
+
+/*for(let pos=0; pos < valores.length; pos++) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}*/
+
